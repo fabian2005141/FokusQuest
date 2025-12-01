d9f6d84c819cbb2b1a093c633970b56189f7340a
fabian2005141 <fabian.l05@icloud.com>
2025-12-01 18:01:34 +0100

    Massage geändert

--- Productivity_Challenge/ContentView.swift
+++ Productivity_Challenge/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Guten Morgen Welt!")
+            Text("Guten Morgen Welt!!!")
         }
         .padding()
     }
