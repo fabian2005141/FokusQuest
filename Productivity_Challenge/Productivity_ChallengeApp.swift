4136c70d6e187cba7361b133c1e837b170ac49f1
fabian2005141 <fabian.l05@icloud.com>
2025-12-01 17:25:15 +0100

    Initial Commit

---
+++ Productivity_Challenge/Productivity_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Productivity_ChallengeApp.swift
+//  Productivity_Challenge
+//
+//  Created by Fabian Lackner on 01.12.25.
+//
+
+import SwiftUI
+
+@main
+struct Productivity_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
